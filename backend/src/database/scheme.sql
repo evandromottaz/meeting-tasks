9370aa2a559dcd7d6d4cc4636e2f67b54f2df2bf
evadromottaz <evandro.motta@live.com>
2025-07-24 16:36:10 -0300

    chore(db): designacoes: add column id PRIMARY KEY, replace primary key to UNIQUE, create VIEW designacoes_info

--- backend/src/database/scheme.sql
+++ backend/src/database/scheme.sql
@@ -52,11 +52,12 @@ CREATE TABLE privilegios (
   UNIQUE(usuario_id, papel_id)
 ) STRICT;
 CREATE TABLE designacoes (
+	id INTEGER PRIMARY KEY,
   data_iso TEXT NOT NULL,
   papel_id INTEGER NOT NULL REFERENCES papeis(id),
   usuario_id INTEGER NOT NULL REFERENCES usuarios(id),
   criado TEXT DEFAULT (datetime('now', '-3 hours')),
-  PRIMARY KEY (data_iso, papel_id, usuario_id)
+  UNIQUE (data_iso, papel_id, usuario_id)
 ) STRICT;
 CREATE TABLE ausencias (
   data_iso TEXT NOT NULL,
@@ -66,4 +67,9 @@ CREATE TABLE ausencias (
   criado TEXT DEFAULT (datetime('now', '-3 hours')),
   FOREIGN KEY (data_iso, papel_id, usuario_id) REFERENCES designacoes(data_iso, papel_id, usuario_id) ON DELETE CASCADE
 ) STRICT;
+CREATE VIEW designacoes_info AS
+SELECT d.id, d.data_iso AS data, p.titulo AS papel, u.nome AS usuario
+FROM designacoes AS d
+INNER JOIN papeis AS p ON p.id = d.papel_id
+INNER JOIN usuarios AS u ON u.id = d.usuario_id;
 COMMIT;
